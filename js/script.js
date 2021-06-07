7ff3a222806a78ef59ad38ebb075414df127d684
maselka <gabdullinmr@gmail.com>
2021-06-07 16:31:05 +0300

    1.       Поправил центрирование
    2.       Поправил шрифту по макету
    
    3.       Убрал не дефолтное расстояние между символами.
    
    4.       Поправил шрифты .
    
    5.       Добавил тень при наведении
    
    6.       Убрал обводку.
    
    7.       Оформил ссылкой
    
    8.       Добавил урезание  и "…"  при переполнении
    
    9.       Добавил урезание  и "…"  при переполнении
    
    10.   Исправил поведение при переполнении
    
    11.   Перенес в стили часть ярлыка лучше цены
    
    12.   Добавил подключение шрифтов из проекта
    
    13.   Убрал пустые теги
    
    14.   Поменял на svj
    
    15.   Убрал тэг article
    Добавил section для блока карточек
    Карточки с номерами оформил как ненумерованный список ul
    
    16.   Поправил стили по БЭМ
    
    17.   Добавил тэг picture
    
    18.   Описал все комплексные свойства раздельно
    
    19.   Добавил кавычки
    
    20.   Переделал на !impotant.
    
    21.   Неиспользуемые стили удалены
    
    22.   Сделал обращение по классу
    
    23.   Оформил ссылкой, добавил поведение

--- js/script.js
+++ js/script.js
@@ -1,20 +1,27 @@
-let reserveButtons = document.getElementsByClassName("reserve_btn");
+let reserveButtons = document.getElementsByClassName("reserve__btn");
 
 for( let i = 0; i < reserveButtons.length; i++) {
     reserveButtons[i].addEventListener("click", reserveButtonHandler);
 }
 
 function reserveButtonHandler (e) {
-    e.target.offsetParent.addEventListener("mouseleave", addReserved);
+    var card = e.path.find(isRoom);
+    card.addEventListener("mouseleave", addReserved);
 }
 
 function addReserved (e) {
-    e.target.classList.add("reserved");
+    e.target.classList.add("room_reserved");
     e.target.removeEventListener("mouseleave", addReserved);
     e.target.addEventListener("click", removeReserved);
 }
 
 function removeReserved (e) {
-    e.currentTarget.classList.remove("reserved")
+    if (e.target.classList.contains("reserved__text_link"))
+        return;
+    e.currentTarget.classList.remove("room_reserved")
     e.currentTarget.removeEventListener("click", removeReserved);
+}
+
+function isRoom (e) {
+    return e.classList.contains("room");
 }
